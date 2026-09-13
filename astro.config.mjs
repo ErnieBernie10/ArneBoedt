3fd18d6682166eaa544688103e6b73aaef44022a
Arne Boedt <arne@boedt.me>
2026-09-13 17:35:23 +0200

    Update amend config again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+const amendBase = process.env.AMEND_BASE;
+
 export default defineConfig({
   compressHTML: true,
+  devToolbar: { enabled: false },
+  ...(amendBase ? { vite: { base: amendBase } } : {}),
 });
